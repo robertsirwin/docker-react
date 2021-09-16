276152cfb29d4e3c09d28132075d33b9712b9f80
Eric Roberts <ericroberts@Erics-MacBook-Pro.local>
2021-09-16 12:03:58 -0400

    For Chris

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
            Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          ER was here!!!
+          HEY BOSS I DID IT!!!  I'm getting there!!!  It's all coming together!!!
         </p>
         <a
           className="App-link"
